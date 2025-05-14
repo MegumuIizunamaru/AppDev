e6a06837255624232b4ce9057008ced8f569c1a0
MegumuIizunamaru <chopin.won@gmail.com>
2025-05-14 17:02:04 +0700

    Pet Page update.

--- app/(nav)/petpage.tsx
+++ app/(nav)/petpage.tsx
@@ -1,23 +1,33 @@
 
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image, Text, Button } from 'react-native';
 import React from 'react';
 import { getSelectedPet } from '@/components/Pet';
 import { Card } from 'react-native-paper'
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { ThemedText } from '@/components/ThemedText';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { white } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
+import EditPetScreen from '@/components/UpdatePet';
 // Can you put style on ThemedText?
 export default function PetPage() {
     const item = getSelectedPet();
     const colorScheme = useColorScheme();
+    const c = colorScheme === 'dark' ? '#FFF' : '#000'
     //FLAT LIST  GET PETS RETURN LIST
-    return (<View>
-        <Card>
-            <Card.Content>
-                <ThemedText>{item?.type}, Breed: {item?.breed}</ThemedText>
-                <ThemedText>Gender: {item?.sex}, Weight: {item?.weight}</ThemedText>
-            </Card.Content>
-
-        </Card>
+    return (<View style={[styles.container, { borderColor: c }]}>
+        <ThemedText type="title" style={styles.text}>{item?.name}</ThemedText>
+        <Image
+            source={{ uri: item?.picture }}
+            style={styles.image}
+            resizeMode='stretch' />
+        <View style={{ alignContent: 'center', margin: 10 }}>
+            <ThemedText type="subtitle" style={styles.text}>
+                {`Type: ${item?.type}\nBreed: ${item?.breed}\nGender: ${item?.sex}\nWeight: ${item?.weight} Kilogram`}
+            </ThemedText>
+            <View style={{ marginTop: 15, width: 100, alignSelf: 'center' }}>
+                <Button title="Edit Info" onPress={() => console.log(item?._id)} />
+            </View>
+        </View>
     </View>
     )
 
@@ -25,6 +35,18 @@ export default function PetPage() {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        alignSelf: 'center', borderWidth: 3, borderRadius: 15, marginTop: 10
+    },
+    image: {
+        width: 280,
+        height: 280,
+        borderRadius: 140,
+        margin: 10
+    },
+    text: {
+        alignSelf: 'center'
+    },
     titleContainer: {
         flexDirection: 'row',
         alignItems: 'center',
